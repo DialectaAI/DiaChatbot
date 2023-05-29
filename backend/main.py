5ffdddba1a8bc73f0bf4b3952aa32b98b73977ce
Alexander Yao <9095664+AlexKYao@users.noreply.github.com>
2023-05-29 15:22:52 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,12 +26,7 @@ app = FastAPI()
 
 # CORS - Origins
 origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:4173",
-    "http://localhost:4174",
-    "http://localhost:3000",
-    "http://localhost:8000",
+    "*",
 ]
 
 
